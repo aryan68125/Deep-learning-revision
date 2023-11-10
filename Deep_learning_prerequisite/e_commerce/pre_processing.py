8cb58f4110f0d4fa44a323c83f55f4751faed9f9
Aditya Kumar <aryan68125@gmail.com>
2023-11-10 15:38:29 +0530

    pre-processing e-commerce

--- Deep_learning_prerequisite/e_commerce/pre_processing.py
+++ Deep_learning_prerequisite/e_commerce/pre_processing.py
@@ -1,3 +1,39 @@
-import numpy as np
+import numpy as numpy
 import pandas as pd
+from tabulate import tabulate
+import matplotlib as plot
 import os
+
+'''
+get data function
+'''
+def get_data():
+   # load our data by reading the csv file in the file manager
+   df = pd.read_csv('ecommerce_data.csv')
+   #read the first few entries in the dataframe after reading the csv file
+   '''df.head() will only work in a jupyter or google colab notebook.
+    It won't show output in your terminal'''
+   print(tabulate(df, headers='keys', tablefmt='psql'))
+
+   '''
+   In this dataframe ecommerce_data.csv the user_action column is our target and every other
+   columns in this dataframe is our input or features that we are gonna feed into our ML model
+   '''
+   
+   #displaying histogram of different columns in the dataframe
+   '''
+   The histogram shows the distribution of values in the column time_of_day in the dataframe
+   '''
+   df['time_of_day'].plot(kind='hist')
+   plot.pyplot.show()
+
+   '''
+   It's much more easier to work with numpy arrays. hence we have to convert this 
+   dataframe into a numpy array.
+   data = df.to_numpy() right now as of 2023 This function converts a dataframe into 
+   a numpy array so in the older code you migh see that in order to convert a dataframe 
+   there people used to write 'df.as_matrix()' or 'df.values()' it all means the same
+   i.e these all are converting a dataframe into a numpy array.
+   '''
+   data = df.to_numpy()
+get_data()
