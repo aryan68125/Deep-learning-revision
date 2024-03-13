61dd47eb3fa3ed52d9a820cfc51e6568ffe034b2
Aditya Kumar <aryan68125@gmail.com>
2024-03-13 21:59:10 +0530

    plotting logistic regression calculation of outputs

--- Deep_learning_prerequisite/calculate_output_logistic_regression/logistic.py
+++ Deep_learning_prerequisite/calculate_output_logistic_regression/logistic.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 '''
 So the first thing is we are gonna need some data in order to do logistic regression on.
 '''
@@ -27,4 +28,30 @@ z = Xb.dot(w)
 
 def sigmoid(z):
     return 1/(1+np.exp(-z))
-print (sigmoid(z))
+y = sigmoid(z)
+print (y)
+
+# Plotting
+'''
+plt.scatter: This function creates a scatter plot.
+x[:,0] and x[:,1]: These are the x and y coordinates of the data points. 
+x[:,0] represents all rows of the first column of the x array, and x[:,1] represents all rows of the second column of the x array. 
+This is because x is a 2D array with shape (N, D), where N is the number of samples and D is the number of features.
+c=y: This parameter specifies the color of each data point. Here, y represents the probabilities calculated by the logistic regression model. 
+It assigns colors to the data points based on these probabilities.
+cmap='bwr': This parameter specifies the colormap to be used for coloring the data points. Here, 'bwr' stands for blue-white-red colormap, 
+which maps low values to blue, medium values to white, and high values to red.
+plt.xlabel: This function sets the label for the x-axis of the plot. Here, it sets the label to 'X1'.
+plt.ylabel: This function sets the label for the y-axis of the plot. Here, it sets the label to 'X2'.
+plt.title: This function sets the title of the plot. Here, it sets the title to 'Logistic Regression Result'.
+plt.colorbar: This function adds a colorbar to the plot, which shows the mapping between colors and values. 
+Here, it adds a colorbar and sets its label to 'Probability', 
+indicating that the colors represent the probabilities calculated by the logistic regression model.
+plt.show: This function displays the plot on the screen. It should be called after setting up all the elements of the plot.
+'''
+plt.scatter(x[:,0], x[:,1], c=y, cmap='RdYlGn_r')
+plt.xlabel('X1')
+plt.ylabel('X2')
+plt.title('Logistic Regression Result')
+plt.colorbar(label='Probability')
+plt.show()
